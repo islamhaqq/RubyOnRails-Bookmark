a2df2cb17ba3cabd88ac9b1365a7989ee86ffd89
Syed Aman <sna274@nyu.edu>
2016-11-29 16:36:47 -0500

    set host as actual host of application for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ RubyonrailsBookmark::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # In production host should be set to the actual host of your application
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
