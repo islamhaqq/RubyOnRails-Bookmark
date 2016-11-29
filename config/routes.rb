2ce209f2442c7ed90c20511efddac3c47e7969d8
Syed Aman <sna274@nyu.edu>
2016-11-29 14:26:16 -0500

    generate bookmark resource scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RubyonrailsBookmark::Application.routes.draw do
+  resources :bookmarks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
