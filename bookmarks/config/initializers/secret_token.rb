ecbb77d2d79d3ad6affce0b5a2099f03027b260e
Syed Aman <sna274@nyu.edu>
2016-11-28 20:29:51 -0500

    generate rails proj w/ SQLite db w/out unit tests

---
+++ bookmarks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = '2c283aba7bb47c09fde9b7b431334d178099bf9ebd0caecefde66fd49fb9886614f93fae615b3ba0012f537c0bd3f7e519cf6a1cfd77b73d56769e1a8d9df965'
